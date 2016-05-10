29729b14b9fc5bb1f3e335c55db0363cc526afd1
AntoineHendrickx <antoinehendrickx1995@gmail.com>
2016-05-10 20:52:26 +0200

    Updated sql file

--- sql/import.sql
+++ sql/import.sql
@@ -3,9 +3,9 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Gegenereerd op: 09 mei 2016 om 13:31
--- Serverversie: 10.1.9-MariaDB
--- PHP-versie: 5.6.15
+-- Generation Time: May 10, 2016 at 08:44 PM
+-- Server version: 10.1.9-MariaDB
+-- PHP Version: 5.6.15
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -23,7 +23,7 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Tabelstructuur voor tabel `comments`
+-- Table structure for table `comments`
 --
 
 DROP TABLE IF EXISTS `comments`;
@@ -35,10 +35,18 @@ CREATE TABLE `comments` (
   `posttime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `comments`
+--
+
+INSERT INTO `comments` (`c_id`, `comment`, `idPost`, `idUser`, `posttime`) VALUES
+(48, 'Very nice', 23, 3, '2016-05-09 11:35:55'),
+(49, 'Zzz', 8, 3, '2016-05-10 12:12:24');
+
 -- --------------------------------------------------------
 
 --
--- Tabelstructuur voor tabel `followrequests`
+-- Table structure for table `followrequests`
 --
 
 DROP TABLE IF EXISTS `followrequests`;
@@ -50,7 +58,7 @@ CREATE TABLE `followrequests` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 --
--- Gegevens worden geëxporteerd voor tabel `followrequests`
+-- Dumping data for table `followrequests`
 --
 
 INSERT INTO `followrequests` (`fr_id`, `FollowingId`, `FollowedId`, `accepted`) VALUES
@@ -65,7 +73,7 @@ INSERT INTO `followrequests` (`fr_id`, `FollowingId`, `FollowedId`, `accepted`) 
 -- --------------------------------------------------------
 
 --
--- Tabelstructuur voor tabel `follows`
+-- Table structure for table `follows`
 --
 
 DROP TABLE IF EXISTS `follows`;
@@ -76,7 +84,7 @@ CREATE TABLE `follows` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 --
--- Gegevens worden geëxporteerd voor tabel `follows`
+-- Dumping data for table `follows`
 --
 
 INSERT INTO `follows` (`f_id`, `idFollowing`, `idFollowed`) VALUES
@@ -118,7 +126,7 @@ INSERT INTO `follows` (`f_id`, `idFollowing`, `idFollowed`) VALUES
 -- --------------------------------------------------------
 
 --
--- Tabelstructuur voor tabel `likes`
+-- Table structure for table `likes`
 --
 
 DROP TABLE IF EXISTS `likes`;
@@ -129,7 +137,7 @@ CREATE TABLE `likes` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 --
--- Gegevens worden geëxporteerd voor tabel `likes`
+-- Dumping data for table `likes`
 --
 
 INSERT INTO `likes` (`l_id`, `likerID`, `postLikedId`) VALUES
@@ -175,12 +183,13 @@ INSERT INTO `likes` (`l_id`, `likerID`, `postLikedId`) VALUES
 (74, 10, 15),
 (75, 10, 22),
 (76, 10, 9),
-(77, 10, 21);
+(77, 10, 21),
+(95, 3, 33);
 
 -- --------------------------------------------------------
 
 --
--- Tabelstructuur voor tabel `posts`
+-- Table structure for table `posts`
 --
 
 DROP TABLE IF EXISTS `posts`;
@@ -189,40 +198,67 @@ CREATE TABLE `posts` (
   `picture` varchar(255) COLLATE utf8_bin NOT NULL,
   `description` varchar(300) COLLATE utf8_bin DEFAULT NULL,
   `posttime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `Report` int(5) NOT NULL,
-  `idUser` int(11) NOT NULL
+  `idUser` int(11) NOT NULL,
+  `location` varchar(300) COLLATE utf8_bin DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 --
--- Gegevens worden geëxporteerd voor tabel `posts`
+-- Dumping data for table `posts`
 --
 
-INSERT INTO `posts` (`p_id`, `picture`, `description`, `posttime`, `Report`, `idUser`) VALUES
-(1, 'images/posts/admin-1461439777-thegym.jpg', 'The fabled creativity gym', '2016-04-15 22:00:00', 0, 1),
-(2, 'images/posts/robinreyns-1461439899-superbrein.jpg', 'Superbrein, super programma!', '2016-04-16 22:00:00', 0, 2),
-(3, 'images/posts/antoinehendrickx-1461439979-akira.jpg', 'Akira is a great movie. ', '2016-04-17 22:00:00', 0, 3),
-(4, 'images/posts/jorishens-1461440193-stempeltje-fb.jpg', 'My new app: stempeltje!', '2016-04-18 22:00:00', 0, 4),
-(5, 'images/posts/michaelrosen-1461440268-rosen book.jpg', 'My new book is out!', '2016-04-19 22:00:00', 0, 5),
-(6, 'images/posts/lucina-1461440437-Awakening_Map.jpg', 'This map is huge!', '2016-04-20 22:00:00', 0, 6),
-(7, 'images/posts/chrom-1461440527-fe.jpg', 'Cover art for FE: Awakening.', '2016-04-21 22:00:00', 0, 7),
-(8, 'images/posts/bedje-1461440584-slapen.jpg', 'Want wie slaapt er nu niet graag?', '2016-04-22 22:00:00', 0, 8),
-(9, 'images/posts/dickladder-1461440655-meeting.jpg', 'Straks meeting. Het zit hier al vol.', '2016-04-23 22:00:00', 0, 9),
-(10, 'images/posts/thomasvanmalderen-1461440764-holhorse.jpg', 'A drawing I made.', '2016-04-24 22:00:00', 0, 10),
-(11, 'images/posts/thomasvanmalderen-1461834922-180px-Maestro_logo.png', 'kak', '2016-04-28 09:15:22', 0, 10),
-(14, 'images/posts/thomasvanmalderen-1461849223-aronskelk.jpg', 'aronskelk! Astonishing!', '2016-04-28 13:13:43', 0, 10),
-(15, 'images/posts/thomasvanmalderen-1461849257-12801433_10207549217199914_810943447972314614_n.jpg', 'Dit is Max.', '2016-04-28 13:14:17', 0, 10),
-(17, 'images/posts/thomasvanmalderen-1461849293-bord.jpg', 'een bord. #wawuie', '2016-04-28 13:14:53', 0, 10),
-(18, 'images/posts/thomasvanmalderen-1461849303-12362806_1044898455530272_2426150736144359618_o.jpg', 'onze prez. #dem', '2016-04-28 13:15:03', 0, 10),
-(19, 'images/posts/admin-1461850578-375051_445059205587216_1606907518_n.jpg', 'IMD''er Vincent!', '2016-04-28 13:36:18', 0, 1),
-(20, 'images/posts/admin-1461850587-11894523_1037589702931490_832188887331351_o.jpg', 'IMD''er Yaron!', '2016-04-28 13:36:27', 0, 1),
-(21, 'images/posts/admin-1461850596-12362806_1044898455530272_2426150736144359618_o.jpg', 'IMD''er Damon!', '2016-04-28 13:36:36', 0, 1),
-(22, 'images/posts/admin-1461850605-12801433_10207549217199914_810943447972314614_n.jpg', 'IMD''er Max!', '2016-04-28 13:36:45', 0, 1),
-(23, 'images/posts/michaelrosen-1462649943-Bohalista-simple-white-bedroom-7.jpg', 'This is a nice bedroom!', '2016-05-07 19:39:03', 0, 5);
+INSERT INTO `posts` (`p_id`, `picture`, `description`, `posttime`, `idUser`, `location`) VALUES
+(1, 'images/posts/admin-1461439777-thegym.jpg', 'The fabled creativity gym', '2016-04-15 22:00:00', 1, ''),
+(2, 'images/posts/robinreyns-1461439899-superbrein.jpg', 'Superbrein, super programma!', '2016-04-16 22:00:00', 2, ''),
+(3, 'images/posts/antoinehendrickx-1461439979-akira.jpg', 'Akira is a great movie. ', '2016-04-17 22:00:00', 3, ''),
+(4, 'images/posts/jorishens-1461440193-stempeltje-fb.jpg', 'My new app: stempeltje!', '2016-04-18 22:00:00', 4, ''),
+(5, 'images/posts/michaelrosen-1461440268-rosen book.jpg', 'My new book is out!', '2016-04-19 22:00:00', 5, ''),
+(6, 'images/posts/lucina-1461440437-Awakening_Map.jpg', 'This map is huge!', '2016-04-20 22:00:00', 6, ''),
+(7, 'images/posts/chrom-1461440527-fe.jpg', 'Cover art for FE: Awakening.', '2016-04-21 22:00:00', 7, ''),
+(8, 'images/posts/bedje-1461440584-slapen.jpg', 'Want wie slaapt er nu niet graag?', '2016-04-22 22:00:00', 8, ''),
+(9, 'images/posts/dickladder-1461440655-meeting.jpg', 'Straks meeting. Het zit hier al vol.', '2016-04-23 22:00:00', 9, ''),
+(10, 'images/posts/thomasvanmalderen-1461440764-holhorse.jpg', 'A drawing I made.', '2016-04-24 22:00:00', 10, ''),
+(11, 'images/posts/thomasvanmalderen-1461834922-180px-Maestro_logo.png', 'Smax', '2016-04-28 09:15:22', 10, ''),
+(14, 'images/posts/thomasvanmalderen-1461849223-aronskelk.jpg', 'aronskelk! Astonishing!', '2016-04-28 13:13:43', 10, ''),
+(15, 'images/posts/thomasvanmalderen-1461849257-12801433_10207549217199914_810943447972314614_n.jpg', 'Dit is Max.', '2016-04-28 13:14:17', 10, ''),
+(17, 'images/posts/thomasvanmalderen-1461849293-bord.jpg', 'een bord. #wawuie', '2016-04-28 13:14:53', 10, ''),
+(18, 'images/posts/thomasvanmalderen-1461849303-12362806_1044898455530272_2426150736144359618_o.jpg', 'onze prez. #dem', '2016-04-28 13:15:03', 10, ''),
+(19, 'images/posts/admin-1461850578-375051_445059205587216_1606907518_n.jpg', 'IMD''er Vincent!', '2016-04-28 13:36:18', 1, ''),
+(20, 'images/posts/admin-1461850587-11894523_1037589702931490_832188887331351_o.jpg', 'IMD''er Yaron!', '2016-04-28 13:36:27', 1, ''),
+(21, 'images/posts/admin-1461850596-12362806_1044898455530272_2426150736144359618_o.jpg', 'IMD''er Damon!', '2016-04-28 13:36:36', 1, ''),
+(22, 'images/posts/admin-1461850605-12801433_10207549217199914_810943447972314614_n.jpg', 'IMD''er Max!', '2016-04-28 13:36:45', 1, ''),
+(23, 'images/posts/michaelrosen-1462649943-Bohalista-simple-white-bedroom-7.jpg', 'This is a nice bedroom!', '2016-05-07 19:39:03', 5, ''),
+(29, 'images/posts/antoinehendrickx-1462883773-Profiel01.jpeg', 'schoen', '2016-05-10 12:36:13', 3, 'Mechelen, Belgium'),
+(30, 'images/posts/antoinehendrickx-1462883819-Doge&Jerry.jpg', 'azeerrrtetyetgd', '2016-05-10 12:36:59', 3, 'Mechelen, Belgium'),
+(31, 'images/posts/antoinehendrickx-1462903249-Doge Ross.jpg', 'lol', '2016-05-10 18:00:49', 3, NULL),
+(32, 'images/posts/antoinehendrickx-1462903757-me.jpg', '#selfie', '2016-05-10 18:09:17', 3, NULL),
+(33, 'images/posts/antoinehendrickx-1462903935-noice.gif', 'noice', '2016-05-10 18:12:15', 3, 'Mechelen, Belgium');
 
 -- --------------------------------------------------------
 
 --
--- Tabelstructuur voor tabel `users`
+-- Table structure for table `reports`
+--
+
+DROP TABLE IF EXISTS `reports`;
+CREATE TABLE `reports` (
+  `r_id` int(11) UNSIGNED NOT NULL,
+  `reporter` varchar(300) NOT NULL,
+  `reportedPost` varchar(300) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `reports`
+--
+
+INSERT INTO `reports` (`r_id`, `reporter`, `reportedPost`) VALUES
+(3, '7', '33'),
+(4, '3', '33'),
+(5, '10', '33');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
 --
 
 DROP TABLE IF EXISTS `users`;
@@ -239,13 +275,13 @@ CREATE TABLE `users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 --
--- Gegevens worden geëxporteerd voor tabel `users`
+-- Dumping data for table `users`
 --
 
 INSERT INTO `users` (`u_id`, `username`, `firstname`, `lastname`, `email`, `password`, `account`, `avatar`, `bio`) VALUES
 (1, 'admin', 'IMDstagram', 'admin', 'admin@email.com', '$2y$12$mRcMoJYOWErYnynVqqhVA.T.R4n3CJguLmtEuG6DgKH11jHheMuEK', 'private', 'images/avatars/basic_avatar.jpg', 'This is the official page of IMDstagram. Try posting some pictures!'),
 (2, 'robinreyns', 'Robin', 'Reyns', 'robin@email.com', '$2y$12$.FarmgfPWxYVtvw/YWMP3eTphLeLZZ16G7NV.b.2W7ndCifpO63ny', 'public', 'images/avatars/robinreyns-1461439863-robin.jpg', 'Robin Reyns from Thomas MORE. Hello!'),
-(3, 'antoinehendrickx', 'Antoine', 'Hendrickx', 'antoine@email.com', '$2y$12$eIrRKIO6n.FsslGlSxpjju8oifTRnXsJUOREc40TU3gUQy1HtfsZ6', 'public', 'images/avatars/antoinehendrickx-1461439948-antoinehendrickx-1461312837-me.jpg', 'Antoine here. I love loempia''s! '),
+(3, 'antoinehendrickx', 'Antoine', 'Hendrickx', 'antoine@email.com', '$2y$12$6CaakJWjMDyadS5.xVC6ZevXF9d2LBCqJj0N4XZRcz15C/PrstTSu', 'public', 'images/avatars/antoinehendrickx-1462882299-me.jpg', 'Antoine here. I love loempia''s! '),
 (4, 'jorishens', 'Joris', 'Hens', 'joris@email.com', '$2y$12$BWKfcUJOWQ0zjzzaens/N.9MKKXh7lvWKZSL0XXhFds.qgZB4w55K', 'public', 'images/avatars/jorishens-1461440167-goodbytes_thumb.jpg', 'Joris Hens, also known as Goodbytes. Now on IMDstagram!'),
 (5, 'michaelrosen', 'Michael', 'Rosen', 'michael@email.com', '$2y$12$/XOhHsQF1DQ1XMZZEubh2eVBeV4cPmGIQ.qapzN21xtWRCYhwneNS', 'private', 'images/avatars/michaelrosen-1461440246-michael.png', 'Michael rosen. Child books author extraordinaire.'),
 (6, 'lucina', 'Lucina', 'FE', 'lucina@fireemblem.com', '$2y$12$xuyzVnWyqkdm0QXY/z8lGe3o5.d9S.EX8AUoCim02tOpOmDRczLeq', 'public', 'images/avatars/lucina-1461440322-lucina.jpg', 'Lucina from Fire Emblem Awakening. '),
@@ -256,76 +292,87 @@ INSERT INTO `users` (`u_id`, `username`, `firstname`, `lastname`, `email`, `pass
 (11, 'grandillusions', 'Tim', 'Rowett', 'tim@toys.uk', '$2y$12$0P6G3p5nncR3eE4J9VdHveV6UNu7VJVQhMBb6A6FFm0ssz7qIS5FK', 'private', 'images/avatars/grandillusions-1462650171-Rowett-Tim.jpg', 'Boss at Grand Illusions. I love toys! Wow! astonishing! extraordinary! *heh*');
 
 --
--- Indexen voor geëxporteerde tabellen
+-- Indexes for dumped tables
 --
 
 --
--- Indexen voor tabel `comments`
+-- Indexes for table `comments`
 --
 ALTER TABLE `comments`
   ADD PRIMARY KEY (`c_id`);
 
 --
--- Indexen voor tabel `followrequests`
+-- Indexes for table `followrequests`
 --
 ALTER TABLE `followrequests`
   ADD PRIMARY KEY (`fr_id`);
 
 --
--- Indexen voor tabel `follows`
+-- Indexes for table `follows`
 --
 ALTER TABLE `follows`
   ADD PRIMARY KEY (`f_id`);
 
 --
--- Indexen voor tabel `likes`
+-- Indexes for table `likes`
 --
 ALTER TABLE `likes`
   ADD PRIMARY KEY (`l_id`);
 
 --
--- Indexen voor tabel `posts`
+-- Indexes for table `posts`
 --
 ALTER TABLE `posts`
   ADD PRIMARY KEY (`p_id`);
 
 --
--- Indexen voor tabel `users`
+-- Indexes for table `reports`
+--
+ALTER TABLE `reports`
+  ADD PRIMARY KEY (`r_id`);
+
+--
+-- Indexes for table `users`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`u_id`);
 
 --
--- AUTO_INCREMENT voor geëxporteerde tabellen
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT voor een tabel `comments`
+-- AUTO_INCREMENT for table `comments`
 --
 ALTER TABLE `comments`
-  MODIFY `c_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=48;
+  MODIFY `c_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=50;
 --
--- AUTO_INCREMENT voor een tabel `followrequests`
+-- AUTO_INCREMENT for table `followrequests`
 --
 ALTER TABLE `followrequests`
   MODIFY `fr_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 --
--- AUTO_INCREMENT voor een tabel `follows`
+-- AUTO_INCREMENT for table `follows`
 --
 ALTER TABLE `follows`
   MODIFY `f_id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=41;
 --
--- AUTO_INCREMENT voor een tabel `likes`
+-- AUTO_INCREMENT for table `likes`
 --
 ALTER TABLE `likes`
-  MODIFY `l_id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=78;
+  MODIFY `l_id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=96;
 --
--- AUTO_INCREMENT voor een tabel `posts`
+-- AUTO_INCREMENT for table `posts`
 --
 ALTER TABLE `posts`
-  MODIFY `p_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+  MODIFY `p_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
 --
--- AUTO_INCREMENT voor een tabel `users`
+-- AUTO_INCREMENT for table `reports`
+--
+ALTER TABLE `reports`
+  MODIFY `r_id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+--
+-- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
   MODIFY `u_id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
